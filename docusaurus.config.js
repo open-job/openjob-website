265a05c6e2f87bf330d84cca7c932cdf27213486
JasonYHZ <coderyu2018@gmail.com>
2023-05-17 14:34:01 +0800

    feat: ✨menu

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -24,15 +24,15 @@ const config = {
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://your-docusaurus-test-site.com',
+  url: 'https://openjob.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+  organizationName: 'open-job', // Usually your GitHub org/user name.
+  projectName: 'openjob-website', // Usually your repo name.
 
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
@@ -45,7 +45,8 @@ const config = {
     locales: ['en', 'zh-Hans'],
     localeConfigs: {
       'zh-Hans': {
-        htmlLang: 'zh-Hans'
+        htmlLang: 'zh-Hans',
+        label: '简体中文'
       }
     }
 
@@ -61,14 +62,14 @@ const config = {
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+            'https://github.com/open-job/openjob-website/edit/main/',
         },
         blog: {
           showReadingTime: true,
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+            'https://github.com/open-job/openjob-website/edit/main/',
         },
         theme: {
           customCss: require.resolve('./src/css/custom.css'),
